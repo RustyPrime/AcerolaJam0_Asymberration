5f98f23c619fb971efd05acebc283e3b1249d0ac
RustyPrime <rustyprime@outlook.com>
2024-03-08 21:13:40 +0100

    added: screenshot for vfx

---
+++ Scripts/vfx_texture_grabber.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@export var vfx : GPUParticles3D
+@onready var camera = $Camera3D
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		var image = get_viewport().get_texture().get_image()
+		image.flip_y()
+		image.save_png("Textures/"+vfx.name+".png")
+	
